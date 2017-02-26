edb405d8d58a5b23eb0679508bf1c84721e2c4c3
Wisbell <westleyisbell@gmail.com>
2017-02-26 14:31:30 -0600

    fixed spacing error

--- lcd_test.py
+++ lcd_test.py
@@ -1,10 +1,10 @@
-# _____ _____ _____ __ __ _____ _____ 
+# _____ _____ _____ __ __ _____ _____
 #|     |   __|     |  |  |     |     |
 #|  |  |__   |  |  |_   _|  |  |  |  |
 #|_____|_____|_____| |_| |_____|_____|
 #
 # Project Tutorial Url:http://osoyoo.com/?p=1031
-#  
+#
 import smbus
 import time
 import pyrebase
@@ -13,12 +13,12 @@ from collections import OrderedDict
 
 # Add Firebase Info to script
 config = {
-    "apiKey": "AIzaSyCskxqP_KZuFwl6CtcPCXFlEuqwFNnSdI8",
-    "authDomain": "west-score-keeper.firebaseapp.com",
-    "databaseURL": "https://west-score-keeper.firebaseio.com",
-    "storageBucket": "west-score-keeper.appspot.com",
-    "messagingSenderId": "436613032037"
-  }
+  "apiKey": "AIzaSyCskxqP_KZuFwl6CtcPCXFlEuqwFNnSdI8",
+  "authDomain": "west-score-keeper.firebaseapp.com",
+  "databaseURL": "https://west-score-keeper.firebaseio.com",
+  "storageBucket": "west-score-keeper.appspot.com",
+  "messagingSenderId": "436613032037"
+}
 
 firebase = pyrebase.initialize_app(config)
 
@@ -55,7 +55,7 @@ def lcd_init():
   lcd_byte(0x33,LCD_CMD) # 110011 Initialise
   lcd_byte(0x32,LCD_CMD) # 110010 Initialise
   lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
   lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
   lcd_byte(0x01,LCD_CMD) # 000001 Clear display
   time.sleep(E_DELAY)
@@ -97,49 +97,49 @@ def lcd_string(message,line):
 
 def main():
   # Main program block
-  
+
   # Test database
   #pi_game = db.child("currentGames").order_by_child("pi").equal_to(True).get() # this get the only game with true
-  
+
   #pi_game_ordered_dict = pi_game.val()
-  
+
   #pi_game_dict = dict(pi_game_ordered_dict)
-  
+
   #print(pi_game_dict)
-  
+
   #pi_game_key = list(pi_game_dict.keys())
   #print(pi_game_key[0])
-  
+
   #print(pi_game_dict[pi_game_key[0]])
-  
+
   #print(pi_game_dict[pi_game_key[0]]['team1Name'])
   #print(pi_game_dict[pi_game_key[0]]['team2Name'])
-  
+
   # Store team names
-  
+
   #team1Name = pi_game_dict[pi_game_key[0]]['team1Name']
   #team2Name = pi_game_dict[pi_game_key[0]]['team2Name']
 
   # var to store game info
   #game = ""
-  
+
   def stream_handler(message):
 	  print(message["event"])
 	  print(message["path"])
-      print(message["data"])
+    print(message["data"])
 
 	  global game
-      global current_state = db.child("currentGames").order_by_child("pi").equal_to(True).get()
+    global current_state = db.child("currentGames").order_by_child("pi").equal_to(True).get()
 
 	  game = message["data"]
-	
+
 	  print(game)
 
 	  #game_info += 1
 
 	  #print(game_info)
-	  
-	  
+
+
 	  #print("function", cool_data)
 	  #cool_data = message["data"]
 
@@ -147,7 +147,7 @@ def main():
 	  #print("cool_data ", cool_data)
 
 	  #stream_data = message["data"]
-	  
+
 	  #data = message["data"]
 	  #get_keys = list(message["data"].keys())
 	  #keys = list(message["data"].keys())
@@ -160,9 +160,9 @@ def main():
   #team1Name = ""
   #team2Name = ""
   #game_info = 0
-  
-	  
-  
+
+
+
   pi_game = db.child("currentGames").order_by_child("pi").equal_to(True).stream(stream_handler)
 
   # Initialise display
@@ -172,33 +172,33 @@ def main():
 
     #cool_data = "what"
     #print("test cool data", cool_data)
-		
+
     # Send some test
     #lcd_string("Created by         <",LCD_LINE_1)
     #lcd_string("Osoyoo.com        <",LCD_LINE_2)
 
     #time.sleep(3)
-  
+
     # Send some more text
     #lcd_string("> Tutorial Url:",LCD_LINE_1)
     #lcd_string("> http://osoyoo.com",LCD_LINE_2)
 
     #time.sleep(3)
-    
+
     #lcd_string("Hey         <",LCD_LINE_1)
     #lcd_string("Test        <",LCD_LINE_2)
-    
+
     #lcd_string(cool_data + "Hey         <", LCD_LINE_1)
     #lcd_string(cool_data, LCD_LINE_2)
 
     time.sleep(10)
-	
+
     print("game_info", game)
     print("state", current_state)
-    
+
     time.sleep(2)
-    
-    
+
+
 
 if __name__ == '__main__':
 
@@ -208,4 +208,3 @@ if __name__ == '__main__':
     pass
   finally:
     lcd_byte(0x01, LCD_CMD)
-
